17fc606b27a1c3bdb3cd74b628805069c3f45bac
Kyle <33899346+PrinceExpert17@users.noreply.github.com>
2021-08-08 11:17:15 +0200

    Update amazon.py

--- amazon.py
+++ amazon.py
@@ -4,11 +4,11 @@ url = "https://www.amazon.com/s?k=nvme&ref=nb_sb_noss"
 
 headers = {"User-Agents": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"}
 
-s = HTMLSession()
+sess = HTMLSession()
 
 
 def getcode(item):
-    r = s.get(f"https://www.amazon.com/s?k={item}&ref=nb_sb_noss", headers=headers)
+    r = sess.get(f"https://www.amazon.com/s?k={item}&ref=nb_sb_noss", headers=headers)
     asins = r.html.find('div.s-main-slot div[data-asin]')
     return [asin.attrs['data-asin'] for asin in asins if asin.attrs['data-asin'] != '']
 
